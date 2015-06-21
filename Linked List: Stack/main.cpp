42556537541cbf14be12cc295ab40119d0a79340
Chris Searcy <chrissearcy@MacBook-Pro.local>
2015-06-21 11:18:46 -0600

    Initial Commit

---
+++ Linked List: Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Linked List: Stack
+//
+//  Created by Chris Searcy on 6/21/15.
+//  Copyright (c) 2015 Chris Searcy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
